03a830448401bb9b8cb7e7e08ef1603ce3186574
Ayamahdy2000 <aya.mahdy512000@gmail.com>
2020-04-01 03:54:26 +0200

    First committo

---
+++ perceptron.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class Perceptron:
+    def sigmoid(self,x):
+        return 1 / ( 1 + np.exp(-x) )
+
+
+    # input dataset
+    training_inputs = np.array([[0,0,1],
+                                [1,1,1],
+                                [1,0,1],
+                                [0,1,1]])
+
+    # output dataset
+    training_outputs = np.array([[0,1,1,0]]).T
+
+    np.random.seed(1)
+
+    random_weights = 2 * np.random.random((3,1))-1
+
+    print("random starting weights:")
+    print("-----------------------")
+    print(random_weights)
+
+    def check(self) :
+        for iterx in range(1):
+            output = np.dot(self.training_inputs , self.random_weights)
+            activiation_output = self.sigmoid(output)
+            print("activiation_output:")
+            print("-------------------")
+            print(activiation_output)
+result= Perceptron()
+result.check()
+
